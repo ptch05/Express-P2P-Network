1039a546e1581fa572bac8fea4ebe3aa15ae6827
Peiman Timaji <peimantimaji@yahoo.com>
2025-04-24 00:02:50 +0100

    finds a random seed server

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getRandomSeedServer } from './src/getRandomSeedServer';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
@@ -8,3 +9,11 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+async function initialize(){
+    const randomSeedServer = getRandomSeedServer();
+    console.log(randomSeedServer);
+}
+
+initialize();
