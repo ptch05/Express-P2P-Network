f9beabc50e23309988f800d55fe6b0a72c34e0c6
Peiman Timaji <peimantimaji@yahoo.com>
2025-04-24 14:05:28 +0100

    added requestid to lookups

--- src/lookupUser.ts
+++ src/lookupUser.ts
@@ -1,10 +1,15 @@
 import fetch from "cross-fetch";
+import { Node } from "./servers";
 
-export function lookupUser(user: string, uri: string){
-    return fetch(`${uri}/lookup?user=${user}`).then(res => {
-        if (res.status !== 200) {
-            throw new Error(`Failed to lookup user ${user} on seed server: ${res.statusText}`);
-        }
-        return res.json();
-    });
+export function lookupUser(user: string, uri: string, requestId: string) {
+  return fetch(`${uri}/lookup?user=${user}`, {
+    headers: {
+      "x-request-id": requestId,
+    },
+  }).then((response) => {
+    if (response.ok) {
+      return response.json() as Promise<Node>;
+    }
+    throw new Error("user not found");
+  });
 }
