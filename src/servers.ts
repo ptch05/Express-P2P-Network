f33a345b883a22c7f60114fbaf0b3e3794db0af4
Peiman Timaji <peimantimaji@yahoo.com>
2025-04-24 14:50:59 +0100

    add in message

--- src/servers.ts
+++ src/servers.ts
@@ -1,23 +1,23 @@
 export type Node = {
-    uri: string;
-    user: string;
-  };
-  
-  const servers: Node[] = [];
-  
-  export function getNodes() {
-    return [...servers];
-  }
-  
-  export function addNode(node: Node) {
-    console.log(`registering ${node.user}`);
-    const isAlreadyAdded = servers.find(
-      (existingNode) => existingNode.user === node.user
-    );
-    if (isAlreadyAdded) return;
-    servers.push(node);
-  }
-  
-  export function getNodeByUser(user: string) {
-    return servers.find((server) => server.user === user);
-  }
+  uri: string;
+  user: string;
+};
+
+const servers: Node[] = [];
+
+export function getNodes() {
+  return [...servers];
+}
+
+export function addNode(node: Node) {
+  console.log(`registering ${node.user}`);
+  const isAlreadyAdded = servers.find(
+    (existingNode) => existingNode.user === node.user
+  );
+  if (isAlreadyAdded) return;
+  servers.push(node);
+}
+
+export function getNodeByUser(user: string) {
+  return servers.find((server) => server.user === user);
+}
