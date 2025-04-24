3a1f4a0a17951973cc613aa3fbe9aa41b4bad22d
Peiman Timaji <peimantimaji@yahoo.com>
2025-04-24 14:03:56 +0100

    clean up register.ts

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,8 +1,11 @@
-import type { Request, Response } from 'express';
-import { addNode } from '../servers';
+import type { Request, Response } from "express";
+import { addNode } from "../servers";
 
-export function register(req: Request, res: Response){
-    const { user, uri } = req.body;
-    addNode({ user, uri });
-    res.status(200).json({ message: "Node registered successfully" });
+export function register(req: Request, res: Response) {
+  const { user, uri } = req.body;
+  addNode({
+    user,
+    uri,
+  });
+  res.json({ message: "success" });
 }
