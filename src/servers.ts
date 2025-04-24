1854b526f06bdbc0ca6b67b76d901bf0c7d24245
Peiman Timaji <peimantimaji@yahoo.com>
2025-04-24 14:04:25 +0100

    fix node naming issue

--- src/servers.ts
+++ src/servers.ts
@@ -1,21 +1,23 @@
 export type Node = {
-    user: string;
     uri: string;
-}
-
-const servers: Node[] = [];
-
-export function addNode(node: Node){
-    const isAlreadyAdded = servers.find(node => node.user === node.user);
-    if(isAlreadyAdded) return;
-    console.log(`${node.user} registered at uri of ${node.uri}`);
-    servers.push(node);
-}
-
-export function getNodeByUser(user: string){
-    return servers.find(server => server.user === user);
-}
-
-export function getNodes(){
+    user: string;
+  };
+  
+  const servers: Node[] = [];
+  
+  export function getNodes() {
     return [...servers];
-}
+  }
+  
+  export function addNode(node: Node) {
+    console.log(`registering ${node.user}`);
+    const isAlreadyAdded = servers.find(
+      (existingNode) => existingNode.user === node.user
+    );
+    if (isAlreadyAdded) return;
+    servers.push(node);
+  }
+  
+  export function getNodeByUser(user: string) {
+    return servers.find((server) => server.user === user);
+  }
